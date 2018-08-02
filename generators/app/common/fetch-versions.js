79077db9b02204669e64bb7cc0d945c85b04a623
semchev <vehcmes@gmail.com>
2018-08-02 12:25:25 -0700

    reduced future code required for additional artifact fetching

--- generators/app/common/fetch-versions.js
+++ generators/app/common/fetch-versions.js
@@ -2,24 +2,28 @@ const request = require('request-promise-native');
 module.exports.kotlin = {};
 
 module.exports.kotlin.fetch = function(context) {
-    const kotlinDefaultFallback = '1.2.51';
-    const kotlinDefaultQuery =
-          'https://search.maven.org/solrsearch/select?q=g:org.jetbrains.kotlin%20AND%20a:kotlin-project&wt=json'
-    
-    const kotlinDefaultErr = () => {
-        context.log('Unable to find latest kotlin version automatically, defaulting to ' + kotlinDefaultFallback);
-        context.kotlinDefault = kotlinDefaultFallback;
+    return fetch(context,
+        'Kotlin',
+        '1.2.51',
+        'https://search.maven.org/solrsearch/select?q=g:org.jetbrains.kotlin%20AND%20a:kotlin-project&wt=json'
+    );
+}
+
+const fetch = (context, artifactName, defaultFallbackVersion, query) => {
+    const defaultErr = () => {
+        context.log('Unable to find latest' + artifactName + ' version automatically, defaulting to ' + defaultFallbackVersion);
+        context[artifactName.toLowerCase() + 'Default'] = defaultFallbackVersion;
     }
 
-    return request(kotlinDefaultQuery)
+    return request(query)
         .then(body => {
             const result = JSON.parse(body);
             if(result.response.docs.length < 1){
-                kotlinDefaultErr(); return;
+                defaultErr(); return;
             }
-            context.kotlinDefault = result.response.docs[0].latestVersion;
+            context[artifactName.toLowerCase() + 'Default'] = result.response.docs[0].latestVersion;
         })
         .catch((err) => {
-            kotlinDefaultErr(); return;
+            defaultErr(); return;
         });
-}
+};
